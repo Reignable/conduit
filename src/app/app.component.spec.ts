dfc299af43b4fc3944b401fc7eaa80caf3371fb0
Reignable <joebarrett401@gmail.com>
2024-02-08 17:33:01 +0000

    Create better example app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,41 @@
 import { render, screen } from '@testing-library/angular'
 import { AppComponent } from './app.component'
+import userEvent from '@testing-library/user-event'
 
 describe('AppComponent', () => {
-  it('should create the app', async () => {
-    const fixture = await render(AppComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app).toBeTruthy()
-  })
-
-  it(`should have the 'angular-template' title`, async () => {
-    const fixture = await render(AppComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('angular-template')
-  })
-
-  it('should render title', async () => {
+  it('should show the default value of the counter', async () => {
     await render(AppComponent)
-    expect(screen.getByRole('heading').textContent).toContain(
-      'Hello, angular-template',
-    )
+    expect(screen.getByText('Count: 0')).toBeInTheDocument()
+  })
+
+  it('should increment the counter', async () => {
+    await render(AppComponent)
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    expect(await screen.findByText('Count: 1')).toBeInTheDocument()
+  })
+
+  it('should decrement the counter', async () => {
+    await render(AppComponent)
+    userEvent.click(screen.getByRole('button', { name: 'Decrement' }))
+    expect(await screen.findByText('Count: -1')).toBeInTheDocument()
+  })
+
+  it('should reset the counter', async () => {
+    await render(AppComponent)
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    expect(await screen.findByText('Count: 3')).toBeInTheDocument()
+    userEvent.click(screen.getByRole('button', { name: 'Reset' }))
+    expect(await screen.findByText('Count: 0')).toBeInTheDocument()
+  })
+
+  it('should display the square of the value', async () => {
+    await render(AppComponent)
+    expect(screen.getByText('Square: 0')).toBeInTheDocument()
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    expect(await screen.findByText('Square: 1')).toBeInTheDocument()
+    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
+    expect(await screen.findByText('Square: 4')).toBeInTheDocument()
   })
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core'
+
+const DEFAULT_COUNT = 0
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'angular-template'
+  count = signal(DEFAULT_COUNT)
+  square = computed(() => this.count() ** 2)
+
+  increment() {
+    this.count.update(value => value + 1)
+  }
+
+  decrement() {
+    this.count.update(value => value - 1)
+  }
+
+  reset() {
+    this.count.set(DEFAULT_COUNT)
+  }
 }
