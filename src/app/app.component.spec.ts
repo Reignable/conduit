b49e529c8ec179680b4caa8f2f5f5409534639c7
Reignable <joebarrett401@gmail.com>
2024-02-08 17:46:18 +0000

    Add testing-library-selector

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,41 +1,50 @@
-import { render, screen } from '@testing-library/angular'
-import { AppComponent } from './app.component'
+import { render } from '@testing-library/angular'
 import userEvent from '@testing-library/user-event'
+import { byRole, byText } from 'testing-library-selector'
+import { AppComponent } from './app.component'
+
+const ui = {
+  incrementButton: byRole('button', { name: 'Increment' }),
+  decrementButton: byRole('button', { name: 'Decrement' }),
+  resetButton: byRole('button', { name: 'Reset' }),
+  count: (count: number) => byText(`Count: ${count}`),
+  square: (square: number) => byText(`Square: ${square}`),
+}
 
 describe('AppComponent', () => {
   it('should show the default value of the counter', async () => {
     await render(AppComponent)
-    expect(screen.getByText('Count: 0')).toBeInTheDocument()
+    expect(ui.count(0).get()).toBeInTheDocument()
   })
 
   it('should increment the counter', async () => {
     await render(AppComponent)
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    expect(await screen.findByText('Count: 1')).toBeInTheDocument()
+    userEvent.click(ui.incrementButton.get())
+    expect(await ui.count(1).find()).toBeInTheDocument()
   })
 
   it('should decrement the counter', async () => {
     await render(AppComponent)
-    userEvent.click(screen.getByRole('button', { name: 'Decrement' }))
-    expect(await screen.findByText('Count: -1')).toBeInTheDocument()
+    userEvent.click(ui.decrementButton.get())
+    expect(await ui.count(-1).find()).toBeInTheDocument()
   })
 
   it('should reset the counter', async () => {
     await render(AppComponent)
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    expect(await screen.findByText('Count: 3')).toBeInTheDocument()
-    userEvent.click(screen.getByRole('button', { name: 'Reset' }))
-    expect(await screen.findByText('Count: 0')).toBeInTheDocument()
+    userEvent.click(ui.incrementButton.get())
+    userEvent.click(ui.incrementButton.get())
+    userEvent.click(ui.incrementButton.get())
+    expect(await ui.count(3).find()).toBeInTheDocument()
+    userEvent.click(ui.resetButton.get())
+    expect(await ui.count(0).find()).toBeInTheDocument()
   })
 
   it('should display the square of the value', async () => {
     await render(AppComponent)
-    expect(screen.getByText('Square: 0')).toBeInTheDocument()
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    expect(await screen.findByText('Square: 1')).toBeInTheDocument()
-    userEvent.click(screen.getByRole('button', { name: 'Increment' }))
-    expect(await screen.findByText('Square: 4')).toBeInTheDocument()
+    expect(ui.square(0).get()).toBeInTheDocument()
+    userEvent.click(ui.incrementButton.get())
+    expect(await ui.square(1).find()).toBeInTheDocument()
+    userEvent.click(ui.incrementButton.get())
+    expect(await ui.square(4).find()).toBeInTheDocument()
   })
 })
