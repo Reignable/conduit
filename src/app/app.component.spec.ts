5fce302da7261884960918b5aa0a9915a0ae157e
Reignable <joebarrett401@gmail.com>
2024-02-08 17:10:44 +0000

    Add Eslint stylistic

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,23 @@
-import { TestBed } from "@angular/core/testing";
-import { AppComponent } from "./app.component";
-import { render, screen } from "@testing-library/angular";
+import { render, screen } from '@testing-library/angular'
+import { AppComponent } from './app.component'
 
-describe("AppComponent", () => {
-  it("should create the app", async () => {
-    const fixture = await render(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+describe('AppComponent', () => {
+  it('should create the app', async () => {
+    const fixture = await render(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    expect(app).toBeTruthy()
+  })
 
   it(`should have the 'angular-template' title`, async () => {
-    const fixture = await render(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("angular-template");
-  });
+    const fixture = await render(AppComponent)
+    const app = fixture.debugElement.componentInstance
+    expect(app.title).toEqual('angular-template')
+  })
 
-  it("should render title", async () => {
-    await render(AppComponent);
-    expect(screen.getByRole("heading").textContent).toContain(
-      "Hello, angular-template"
-    );
-  });
-});
+  it('should render title', async () => {
+    await render(AppComponent)
+    expect(screen.getByRole('heading').textContent).toContain(
+      'Hello, angular-template',
+    )
+  })
+})
