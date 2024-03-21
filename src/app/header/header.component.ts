b21dd7c8984f321e354e42ac523f07cafe701cdc
Reignable <joebarrett401@gmail.com>
2024-03-21 16:44:58 +0000

    Convert header to signals

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input, computed, signal } from '@angular/core'
 import { User } from '@model'
+import { MenuItem } from 'primeng/api'
+import { MenubarModule } from 'primeng/menubar'
 
 @Component({
   selector: 'conduit-header',
   standalone: true,
   templateUrl: './header.component.html',
+  styles: [`
+  :host ::ng-deep p-menubarsub {
+    width: 100%;
+}
+  `],
+  imports: [MenubarModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  @Input() isLoggedIn = false
-  @Input() token: User['token'] = ''
-  @Input() username: User['username'] = ''
-  @Input() image: User['image'] = ''
+  _isLoggedIn = signal(false)
+  @Input() set isLoggedIn(value: boolean) { this._isLoggedIn.set(value) }
+
+  _token = signal<User['token']>('')
+  @Input() set token(value: User['token']) { this._token.set(value) }
+
+  _username = signal<User['username']>('')
+  @Input() set username(value: User['username']) { this._username.set(value) }
+
+  _image = signal<User['image']>('')
+  @Input() set image(value: User['image']) { this._image.set(value) }
 }
