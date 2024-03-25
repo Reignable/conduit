14210d958056f7fa355af248e482f7202f3cc229
Reignable <joebarrett401@gmail.com>
2024-03-25 09:46:39 +0000

    Add header to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core'
-
-const DEFAULT_COUNT = 0
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { HeaderComponent } from './header/header.component'
 
 @Component({
   selector: 'conduit-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [HeaderComponent],
 })
 export class AppComponent {
-  count = signal(DEFAULT_COUNT)
-  square = computed(() => this.count() ** 2)
-
-  increment() {
-    this.count.update(value => value + 1)
-  }
-
-  decrement() {
-    this.count.update(value => value - 1)
-  }
-
-  reset() {
-    this.count.set(DEFAULT_COUNT)
-  }
 }
