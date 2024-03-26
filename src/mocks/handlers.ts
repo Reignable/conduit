4f6948b0d45d28361f6e8e00ccf6cf14e7a53aa3
Reignable <joebarrett401@gmail.com>
2024-03-26 10:21:54 +0000

    Add mocks index

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,22 +3,9 @@ import {
 } from 'msw'
 import {
   ErrorResponse,
-  RegisterRequest, User, UserResponse,
+  RegisterRequest, UserResponse,
 } from '@model'
-import { faker } from '@faker-js/faker'
-
-const createUser = (overrides: Partial<User> = {}): User => {
-  const firstName = faker.person.firstName()
-  const lastName = faker.person.lastName()
-  return ({
-    bio: faker.lorem.sentence(),
-    image: faker.image.avatar(),
-    token: faker.string.uuid(),
-    email: faker.internet.email({ firstName, lastName }),
-    username: faker.internet.userName({ firstName, lastName }),
-    ...overrides,
-  })
-}
+import { createUser } from './createUser'
 
 const registerHandler = http.post<PathParams, RegisterRequest>('/users',
   async ({ request }) => {
